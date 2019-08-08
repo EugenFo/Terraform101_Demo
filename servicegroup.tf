edd8637ed0bf326321f1c38c216b34cb45060874
Eugen <eugen.fohlenweider@hotmail.com>
2019-08-08 21:21:14 +0200

    added new sg for elb

--- servicegroup.tf
+++ servicegroup.tf
@@ -22,3 +22,23 @@ resource "aws_security_group" "sg_demo" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
+
+
+resource "aws_security_group" "sg_elb" {
+  name = "allow_http_to_elb"
+  vpc_id = "${aws_vpc.vpc_demo.id}"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 65000
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
